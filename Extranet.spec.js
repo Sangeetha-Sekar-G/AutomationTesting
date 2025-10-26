4a627cf21bfa81256ac18ec4ed1b367985c07e8f
Sangeetha Sekar <sangeetha0899@gmail.com>
2025-10-26 20:28:44 +0100

    Update policy number in Extranet test case

--- Extranet.spec.js
+++ Extranet.spec.js
@@ -4,15 +4,14 @@ const {test, expect} = require('@playwright/test');
 test('Extranet Policy Cancellation - Search by Contract No', async({page}) =>
 {
 
+  const policyNumber = '3101497595';
   await page.goto('https://extranet-staging.europ-assistance.com/front_v6/Extranet_wd.html#ExtranetLogin:');
   
   await page.locator('input[type="text"]').fill('admin');
   await page.locator('input[type="password"]').fill('admin');
   await page.getByText('OK').click();
   
-  await page.locator('div').filter({ hasText: 'OK' }).nth(5).click();
-
-  await page.getByRole('cell', { name: 'Contract No.' }).getByRole('textbox').fill('3101497592');
+  await page.getByRole('cell', { name: 'Contract No.' }).getByRole('textbox').fill(policyNumber);
   await page.getByText('Reset').click();
   await page.getByText('Search', { exact: true }).click();
 
